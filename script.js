effd35dc21dce3b2477a7540b20da7e2bcaec703
RoxanaTesileanu <roxana.te@web.de>
2025-01-19 09:52:49 +0100

    adding  JavaScript functionality

---
+++ script.js
@@ -0,0 +1,47 @@
+const statusElement= document.getElementById("status");
+let transactionListElement = document.getElementById("transaction-list");
+const formElement= document.getElementById("transaction-form");
+const transactionElement = document.getElementById("transaction-aim");
+const amountElement= document.getElementById("amount");
+
+let transactions= []; 
+
+function updateStatus(){
+    let sum = 0;
+    for (const inputs of transactions) {   
+        console.log(inputs)
+        sum += inputs.amount;
+        console.log(sum)
+        statusElement.textContent= `${sum}`;
+        console.log(statusElement.textContent)
+    }
+       
+        transactions.forEach((inputs) => 
+        {const li = document.createElement('li');
+        li.textContent=inputs.transaction + " : ";
+        li.textContent += inputs.amount;
+        console.log(inputs.transaction)
+        console.log(inputs.amount)
+        transactionListElement.appendChild(li);})
+        
+    }
+    
+
+function addTransaction (e){
+e.preventDefault();
+const transaction = transactionElement.value;
+const amount = parseFloat(amountElement.value);
+
+    transactions.push({transaction,amount}); 
+   
+    updateStatus();
+
+ }
+
+
+
+
+
+
+formElement.addEventListener('submit', addTransaction); //program starts if there is an event e
+updateStatus();
